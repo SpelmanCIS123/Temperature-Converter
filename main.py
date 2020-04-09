052f42040446a34f91428f5b75ddbcd66ffcd5e4
bneal7 <60403771+bneal7@users.noreply.github.com>
2020-04-09 15:33:32 -0500

    Update main.py
    ADDED name, code, and updated comments

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-#Your name goes here
+##BROOKLYN NEAL
 
-#Your algorithm should go here OR you should use comments throughout
+##FAHRENHEIT TO CELSIUS CONVERTER
 
 #Your code goes here
 import tkinter
@@ -16,20 +16,47 @@ class FahrenConverterGUI:
         self.bottom_frame = tkinter.Frame(self.main_window)
 
         #Create widgets for the top frame
-        
+        self.prompt_label = tkinter.Label(self.top_frame,
+                                          text = 'Enter a temperature in fahrenheit:')
+        self.ftemp_entry = tkinter.Entry(self.top_frame,
+                                       width = 10)
         #Pack the top frame's widgets
+        self.prompt_label.pack(side = 'left')
+        self.ftemp_entry.pack(side = 'left')
         
         #Create widgets for the bottom frame
+        self.calc_button = tkinter.Button(self.bottom_frame,
+                                          text = 'Calculate',
+                                          command = self.convert)
         self.quit_button = tkinter.Button(self.bottom_frame,
                                           text='Quit',
                                           command=self.main_window.destroy)
         
-        #Pack the bottom frame's widgets
-        self.quit_button.pack(side='left')
+        #Pack the buttons
+        self.calc_button.pack(side = 'left')
+        self.quit_button.pack(side = 'left')
       
         # Pack the frames
         self.top_frame.pack()
         self.bottom_frame.pack()
-        
-        
+
+        #Enter the tkinter main loop
+        tkinter. mainloop()
+
+    #The convert method is a callback function for the Calculate button
+    def convert(self):
+        #Get the value entered by the user into the ftemp_entry widget
+        ftemp = float(self.ftemp_entry.get())
+
+        #Convert fahrenheit to celsius
+        celsius = (int(ftemp) - 32) * (5/9)
+
+        #Display the results in an info dialog box
+        tkinter.messagebox.showinfo('Results',
+                                 str(ftemp) +
+                                 ' temperature in fahrenhet is equal to ' +
+                                 str(celsius) + ' temperature in celsius.')
+
+
+#Create an instance of the TemperatureConverterGUI class
 temp_converter = FahrenConverterGUI()
